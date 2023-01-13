a826d42d07d90f1fed06ead4329a8129c0fe5259
Jimmy Garcia <Jgarcia3000@gmail.com>
2023-01-12 16:36:09 -0800

    import url polyfill

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import "react-native-url-polyfill/auto";
 import CameraComponent from "./components/Camera";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";

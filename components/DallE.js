0d03f2c9640c841af992285e0466affd7659ff40
Jimmy Garcia <Jgarcia3000@gmail.com>
2023-01-16 19:08:18 -0800

    Made image draggable

--- components/DallE.js
+++ components/DallE.js
@@ -1,10 +1,33 @@
-import { useEffect, useState } from "react";
-import { SafeAreaView, View, Image, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  SafeAreaView,
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  Animated,
+  PanResponder,
+} from "react-native";
 import { OpenAIApi, Configuration } from "openai";
 import { OPENAI_API_KEY } from "@env";
 
 export default function DallE() {
   const [imageUrl, setImageUrl] = useState("");
+  const [test, setTest] = useState("");
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: () => {
+        pan.extractOffset();
+      },
+    })
+  ).current;
 
   const configuration = new Configuration({
     apiKey: OPENAI_API_KEY,
@@ -24,29 +47,46 @@ export default function DallE() {
     }
   };
 
+  const setLemon = () => {
+    setTest(
+      "https://vignette.wikia.nocookie.net/joke-battles/images/4/40/18360-doge-doge-simple.jpg/revision/latest?cb=20151209161638"
+    );
+  };
+
   useEffect(() => {
-    generateImage();
+    // generateImage();
+    setLemon();
   }, []);
 
   return (
     <SafeAreaView>
-      <View style={styles.imageContainer}>
-        {imageUrl.length > 0 ? (
-          <Image style={styles.image} source={{ uri: imageUrl }} />
+      <Animated.View
+        style={[
+          styles.imageContainer,
+          {
+            transform: [{ translateX: pan.x }, { translateY: pan.y }],
+          },
+        ]}
+        {...panResponder.panHandlers}
+      >
+        {test.length > 0 ? (
+          <Image
+            style={styles.image}
+            source={{
+              uri: test,
+            }}
+          />
         ) : (
           <Text style={styles.titleText}>Sad Face</Text>
         )}
-      </View>
+      </Animated.View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   imageContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%",
+    overflow: "visible",
   },
   image: {
     width: "100%",

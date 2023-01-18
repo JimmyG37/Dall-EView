e3696f21f4c2a697748d0f0ab4a632ddaa6c9984
Jimmy Garcia <Jgarcia3000@gmail.com>
2023-01-17 16:09:35 -0800

    GestureHandlerRootView

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import "react-native-url-polyfill/auto";
 import React, { useState, useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import CameraComponent from "./components/Camera";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CameraComponent />
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <CameraComponent />
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
